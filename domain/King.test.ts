a793c003e1cbe1e7ef56ae0f980fc1dfe719b5a6
Alberto Herrera <albertoherreravargas@gmail.com>
2025-02-04 19:42:48 +0100

    test: King move when there is ally figure

--- domain/King.test.ts
+++ domain/King.test.ts
@@ -2,13 +2,23 @@ import { Game } from "./Game";
 import { King } from "./King";
 import { Square } from "./Square";
 
-describe('King', () => {
-	it('should allow moving the king forward when there is no ally figure in that square', () => {
+describe("King", () => {
+	it("should allow moving the king forward when there is no ally figure in that square", () => {
 		const game = new Game([]);
-		const king = new King(new Square('E', 1), 'white');
+		const king = new King(new Square("E", 1), "white");
 
-		king.move(new Square('E', 2), game);
+		king.move(new Square("E", 2), game);
 
-		expect(king.square).toEqual(new Square('E', 2));
-	})
-})
+		expect(king.square).toEqual(new Square("E", 2));
+	});
+
+	it("should not allow moving the king forward when there is an ally figure in that square", () => {
+		const king = new King(new Square("E", 1), "white");
+		const allyPawn = new King(new Square("E", 2), "white");
+		const game = new Game([king, allyPawn]);
+
+		const action = () => king.move(new Square("E", 2), game);
+
+		expect(action).toThrow(new Error("Invalid move"));
+	});
+});
