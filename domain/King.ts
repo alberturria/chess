02f2adfca625a30c51d1db042086e5a25c60cf1a
Alberto Herrera <albertoherreravargas@gmail.com>
2025-02-04 19:41:28 +0100

    feat: King move when there is no ally figure

--- domain/King.ts
+++ domain/King.ts
@@ -1,3 +1,4 @@
+import { isDefined } from "../ts-utils";
 import { Color, Figure } from "./Figure";
 import { Game } from "./Game";
 import { Square } from "./Square";
@@ -7,10 +8,30 @@ export class King extends Figure {
 		super(square, color, "king");
 	}
 
-	move(square: Square, game: Game): void {
-		throw new Error("Method not implemented.");
+	move(newSquare: Square, game: Game): void {
+		const availableMoves = this.getAvailableMoves(game);
+		const isMoveValid = !!availableMoves?.find((square) => newSquare.equals(square));
+
+		if (!isMoveValid) {
+			throw new Error("Invalid move");
+		}
+		super.square = newSquare;
 	}
-	getAvailableMoves(): Square[] | undefined {
-		throw new Error("Method not implemented.");
+	getAvailableMoves(game: Game): Square[] | undefined {
+		const squaresAround = [
+			this.square.copySquareAbove(),
+			this.square.copySquareBelow(),
+			this.square.copySquareLeft(),
+			this.square.copySquareRight(),
+			this.square.copySquareAbove()?.copySquareLeft(),
+			this.square.copySquareAbove()?.copySquareRight(),
+			this.square.copySquareBelow()?.copySquareLeft(),
+			this.square.copySquareBelow()?.copySquareRight(),
+		].filter(isDefined);
+		const squaresAroundWithoutAllyFigure = squaresAround.filter(
+			(square) => game.getFigureBySquareAndColor(square, this.color) === undefined
+		);
+
+		return squaresAroundWithoutAllyFigure;
 	}
 }
