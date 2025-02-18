238d2806e54a5d3d6f6e7bf2805f65e4ea82c133
Alberto Herrera <albertoherreravargas@gmail.com>
2025-02-18 20:51:25 +0100

    test: add test for initial moves for black knight (with overlapping ally pieces)

--- domain/Knight.test.ts
+++ domain/Knight.test.ts
@@ -1,5 +1,6 @@
 import { Game } from "./Game";
 import { Knight } from "./Knight";
+import { Pawn } from "./Pawn";
 import { Square } from "./Square";
 
 describe("Knight", () => {
@@ -19,5 +20,18 @@ describe("Knight", () => {
 		const moves = knight.getAvailableMoves(game);
 
 		expect(moves).toEqual(expect.arrayContaining([new Square("A", 6), new Square("C", 6)]));
+	});
+
+	it("should not get any move when there are pieces from the same side on the expected possible squares", () => {
+		const knight = new Knight(new Square("B", 1), "white");
+		const firstAllyPawn = new Pawn(new Square("A", 3), "white");
+		const secondAllyPawn = new Pawn(new Square("C", 3), "white");
+		const thirdAllyPawn = new Pawn(new Square("D", 2), "white");
+
+		const game = new Game([knight, firstAllyPawn, secondAllyPawn, thirdAllyPawn]);
+
+		const moves = knight.getAvailableMoves(game);
+
+		expect(moves).toEqual([]);
 	});
 });
